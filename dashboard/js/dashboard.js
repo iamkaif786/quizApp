49c88e2979cc83f25916afc257db15788dbde497
Mohammadkaif Shaikh <66009452+iamkaif786@users.noreply.github.com>
2025-01-20 18:49:02 +0530

    Create question, update and delete all subject

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -27,6 +27,7 @@ logoutBtn.onclick = function () {
 var visibleSubject = document.querySelector('.visible-subject');
 var subjectBtn = document.querySelector('.subject-btn');
 var subjectEl = document.querySelector('.subject');
+var allSubject = [];
 subjectBtn.onclick = function (e) {
     e.preventDefault();
     if (subjectEl.value != '') {
@@ -35,18 +36,170 @@ subjectBtn.onclick = function (e) {
     } else {
         swal("Subject field is empty !", "Please Enter Subject !", "warning");
     }
+    updateSubject();
 }
 
-const newSubject = () => {
+const newSubject = (subject, index) => {
+    var subjectName = subjectEl.value;
+    if (subject) {
+        subjectName = subject.subjectName;
+    }
     visibleSubject.innerHTML += `
-    <div class="d-flex justify-content-between align-items-center">
-        <h3>${subjectEl.value}</h3>
+    <div class="d-flex subject-box justify-content-between align-items-center">
+        <h3 index='${index}'>${subjectName}</h3>
         <div>
-            <i class="fa fa-edit mx-2" style="font-size: 22px;"></i>
-            <i class="fa fa-save mx-2 d-none" style="font-size: 22px;"></i>
-            <i class="fa fa-trash mx-2" style="font-size: 22px;"></i>
+            <i class="fa fa-edit edit-btn mx-2" style="font-size: 22px;"></i>
+            <i class="fa fa-save save-btn mx-2 d-none" style="font-size: 22px;"></i>
+            <i class="fa fa-trash del-btn mx-2" style="font-size: 22px;"></i>
         </div>
     </div>
 
     `;
+    //  start delete button coding
+    var i;
+    var delAllBtn = visibleSubject.querySelectorAll('.del-btn');
+    for (i = 0; i < delAllBtn.length; i++) {
+        delAllBtn[i].onclick = function () {
+            var parent = this.parentElement.parentElement;
+
+            swal({
+                title: "Are you sure?",
+                text: "Once deleted, you will not be able to recover this imaginary file!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        parent.remove();
+                        updateSubject();
+                        swal("Poof! Your imaginary file has been deleted!", {
+                            icon: "success",
+                        });
+                    } else {
+                        swal("Your imaginary file is safe!");
+                    }
+                });
+        }
+    }
+
+    // start update button coding
+    var allEditBtn = visibleSubject.querySelectorAll(".edit-btn");
+    for (i = 0; i < allEditBtn.length; i++) {
+        allEditBtn[i].onclick = function () {
+            var parent = this.parentElement.parentElement;
+            var h3 = parent.getElementsByTagName("H3");
+            var saveBtn = parent.querySelector(".save-btn");
+            h3[0].contentEditable = true;
+            h3[0].focus();
+            this.classList.add('d-none');
+            saveBtn.classList.remove('d-none');
+            saveBtn.onclick = function () {
+                var editedSub = h3[0].innerHTML;
+                var id = h3[0].getAttribute('index');
+                updateSubject(editedSub, id);
+                this.classList.add('d-none');
+                allEditBtn[id].classList.remove('d-none');
+                h3[0].contentEditable = false;
+            }
+        }
+    }
+}
+
+if (localStorage.getItem(brandCode + '_allSubject') != null) {
+    allSubject = JSON.parse(localStorage.getItem(brandCode + '_allSubject'));
+    allSubject.forEach((subject, index) => {
+        newSubject(subject, index);
+    })
+}
+
+function updateSubject(subject, id) {
+    if (subject != undefined && id != undefined) {
+        allSubject[id] = {
+            subjectName: subject
+        }
+    } else {
+        var i;
+        allSubject = [];
+        var subjectBox = visibleSubject.querySelectorAll('.subject-box');
+        for (i = 0; i < subjectBox.length; i++) {
+            var h3 = subjectBox[i].getElementsByTagName('H3');
+            allSubject.push({
+                subjectName: h3[0].innerHTML
+            });
+        }
+    }
+
+    localStorage.setItem(brandCode + '_allSubject', JSON.stringify(allSubject));
+}
+
+// start return subject in question form (Approved Code)
+
+var chooseSubject = document.querySelector('#choose-subject');
+var questionForm = document.querySelector('.question-form');
+var allQuesInput = questionForm.querySelectorAll('INPUT');
+var selectSubject = document.querySelector('#select-subject');
+var allQuestion = [];
+var subject;
+questionForm.onsubmit = (e) => {
+    e.preventDefault();
+    insertQuestionFunc();
+}
+
+const chooseSubjectFunc = () => {
+    allSubject.forEach((subject, index) => {
+        chooseSubject.innerHTML += `        
+        <option value="${subject.subjectName}">${subject.subjectName}</option>
+        `;
+
+        selectSubject.innerHTML += `        
+        <option value="${subject.subjectName}">${subject.subjectName}</option>
+        `;
+    })
+}
+chooseSubjectFunc();
+
+chooseSubject.addEventListener('change', () => {
+    checkSubject();
+    checkSubjectKey();
+});
+
+var firstOption = chooseSubject.querySelectorAll("OPTION")[1];
+
+function checkSubject() {
+    if (chooseSubject.value == "choose subject") {
+        subject = firstOption.value;        
+    } else {
+        subject = chooseSubject.value;
+    }
+}
+checkSubject();
+
+function checkSubjectKey() {
+    if (localStorage.getItem(brandCode + '_' + subject + '_question')!= null) {
+        allQuestion = JSON.parse(localStorage.getItem(brandCode + '_' + subject + '_question'));
+    }else{
+        allQuestion = [];
+    }
+}
+
+checkSubjectKey();
+
+function insertQuestionFunc() {
+    if (chooseSubject.value != "choose subject") {
+        allQuestion.push({
+            question: allQuesInput[0].value,
+            optionOne: allQuesInput[1].value,
+            optionTwo: allQuesInput[2].value,
+            optionThree: allQuesInput[3].value,
+            optionFour: allQuesInput[4].value,
+            correctAnswer: allQuesInput[5].value
+        });
+
+        localStorage.setItem(brandCode + '_' + chooseSubject.value + "_question", JSON.stringify(allQuestion));
+        swal("Success !", "Data Inserted Successfully !", "success");
+        questionForm.reset('');
+    } else {
+        swal("Choose Subject !", "Please Select Subject !", "warning");
+    }
 }
